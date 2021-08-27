2d0a8f24118b0d8fd5f3de8c035061271097f309
glen1986 <escobar.glen@gmail.com>
2021-08-26 22:55:20 -0300

    entrega

--- js/script.js
+++ js/script.js
@@ -1,26 +1,55 @@
-console.log("script")
 function myCallback(value) {
         return `${value} é muito bom!`
-    
-};
-function myCallback2(value){
-    for(let i = 0; i < value.length; i++){
-        value[i]= value[i]+" é muito bom!"
-    }
-        return `${value}`
 }
-
+function myCallback2(value){
+    console.log(value + " e bom")  
+}
+function myCallback3(value){
+    console.log(value + " e melhor que bom")  
+}
 let myArr = ["maçã", "uva", "pera"];
+let myLetters = ['a','b','c','d','d','e','f','f','g','h']
 let newArr = [];
-let retornoForEach = myArr.forEach(myCallback2);
-let retornoNewForEach = newForEach(myArr, myCallback2);
 
-function newForEach() {
+let retornoMap = myArr.map(myCallback);
+let retornoNewMap = newMap(myArr, myCallback);
+
+function newMap() {
     for (let i = 0; i < myArr.length; i++){
         newArr[i] = myArr[i] +' é muito bom!'
     }
     return newArr;
 }
-retornoForEach // Deve retornar ["maçã é muito bom!", "uva é muito bom!", "pera é muito bom!"];
-retornoNewForEach // Deve retornar ["maçã é muito bom!", "uva é muito bom!", "pera é muito bom!"];
-myArr // Deve retornar ["maçã", "uva", "pera"];
+////////////////
+let retornoFill = myArr.map(myCallback);
+let retornoNewFill = newMap(myArr, myCallback);
+
+
+function newFill() {
+    for (let i = 0; i < myArr.length; i++){
+        newArr[i] = myArr[i] +' é muito bom!'
+    }
+    return newArr;
+}
+////////////////
+let retornoForEach = myArr.forEach(myCallback3)
+let retornoNewForEach = newForEach(myArr, myCallback3);
+
+function newForEach(){
+    for (let i = 0; i < myArr.length; i++ ){
+        newArr[i] = myArr[i] + ' é muito bom!'
+    }
+    return newArr
+}
+///////////////////
+
+let retornoSome = myArr.some(a)
+let retornoNewSome = newSome(a, myArr, myCallback);
+
+function newSome(a){
+    for (let i = 0; i < myLetters.length; i++ ){
+        a = myLetters[i]
+    }
+    return true
+}
+///////////////////
