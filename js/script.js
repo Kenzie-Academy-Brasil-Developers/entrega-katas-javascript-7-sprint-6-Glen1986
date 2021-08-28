03b218db2947bcc1cb806a67af6b950f6697af16
glen1986 <escobar.glen@gmail.com>
2021-08-28 19:58:38 -0300

    callback

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
-function myCallback(value) {
-        return `${value} é muito bom!`
-}
-function myCallback2(value){
-    console.log(value + " e bom")  
-}
-function myCallback3( a){
-}
-function myCallback4(a){
-    return a
-}
+
 
 let string = "Hola "
 let myArr = ["maçã", "uva", "pera"];
@@ -16,84 +6,26 @@ let myLetters = ['a','b','c','d','d','e','f','f','g','h']
 let myNumbers = [2,3,5,5,7,6,8,2,4,7]
 let newArr = [];
 
-let retornoMap = myArr.map(myCallback);
-let retornoNewMap = newMap(myArr, myCallback);
-
-function newMap() {
-    for (let i = 0; i < myArr.length; i++){
-        newArr[i] = myArr[i] +' é muito bom!'
+function myCallback(value) {
+    return `${value} é muito bom!`
+}
+function retornoMap(a,myCallback){
+   return a.map(myCallback);
+};
+function newMap(a) {
+    for (let i = 0; i < a.length; i++){
+        newArr[i] = a[i] +' é muito bom!'
     }
     return newArr;
 }
-////////////////
-let retornoFill = myArr.map(myCallback);
-let retornoNewFill = newMap(myArr, myCallback);
-
-
-function newFill() {
-    for (let i = 0; i < myArr.length; i++){
-        newArr[i] = myArr[i] +' é muito bom!'
-    }
-    return newArr;
+let retornoNewMap = newMap
+////////////////////////////////////////////////////////
+function retornoFill(a, b){
+    return a.fill(b)
 }
-////////////////
-let retornoForEach = myArr.forEach(myCallback3)
-let retornoNewForEach = newForEach(myArr, myCallback3);
-
-function newForEach(){
-    for (let i = 0; i < myArr.length; i++ ){
-        newArr[i] = myArr[i] + ' é muito bom!'
+function retornoNewFill(a, b){
+    for( let i = 0; i < a.length; i++ ){
+        a[i] = b
     }
-    return newArr
-}
-///////////////////
-
-let retornoSome = myArr.some(myCallback3)
-let retornoNewSome = newSome(myArr, myCallback3);
-
-function newSome(a){
-    for (let i = 0; i < myLetters.length; i++ ){
-        a = myLetters[i]
-    }
-    return true
-}
-///////////////////
-let retornoReduce = myNumbers.reduce((a,b) => a + b)
-let retornoNewReduce = newReduce(myNumbers);
-
-function newReduce(){
-    let total = 0;
-    for (let i = 0; i < myNumbers.length; i++ ){
-        total += myNumbers[i]
-    }
-    return total
-}
-///////////////////
-
-let retornoFilter = myNumbers.filter((item) => item % 2 === 0)
-let retornoNewFilter = newFilter(myNumbers);
-
-function newFilter(){
-    let total = [];
-    for (let i = 0; i < myNumbers.length; i++ ){
-        if(myNumbers[i] % 2 ===0){
-           total.push(myNumbers[i])
-        }
-    }
-    return total
-}
-
-///////////////////
-
-let retornoConcat = string.concat(myCallback4)
-let retornoNewConcat = newFilter(myNumbers);
-
-function newFilter(){
-    let total = [];
-    for (let i = 0; i < myNumbers.length; i++ ){
-        if(myNumbers[i] % 2 ===0){
-           total.push(myNumbers[i])
-        }
-    }
-    return total
+    return a
 }
